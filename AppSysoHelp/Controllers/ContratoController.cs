56f771d26ecde140f1f4b7f2c874a6fc7587e618
Almir <almir.matos.dev@gmail.com>
2024-08-28 21:34:48 -0400

    [Tarefa] Alteração layout Cadastro e a pagina index com a lista e o modal de criação

---
+++ AppSysoHelp/Controllers/ContratoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppSysoHelp.Controllers
+{
+    public class ContratoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
