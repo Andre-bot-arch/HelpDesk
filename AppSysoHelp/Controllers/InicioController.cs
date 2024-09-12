6e262bb069a01010cdf692cce1aa74a9ae3bd440
Dev Syso Tecnologia <syso.desenvolvimento@gmail.com>
2024-09-11 21:34:50 -0400

    Pagina inicial, portifolio buscando resultado do banco!

--- AppSysoHelp/Controllers/InicioController.cs
+++ AppSysoHelp/Controllers/InicioController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AppSysoHelp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AppSysoHelp.Controllers
 {
     public class InicioController : Controller
     {
+        private readonly IConfiguration _configuration;
+        private readonly HelpdesksysoContext _context;
+
+        public InicioController(IConfiguration configuration, HelpdesksysoContext context)
+        {
+            _configuration = configuration;
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(_context.PlataformasContratos.ToList());
         }
     }
 }
