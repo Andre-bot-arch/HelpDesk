3f2b13f9aa135b507c7c84b32966222f3386d680
Romulo Kaled <romulokaled@gmail.com>
2024-10-10 15:06:31 -0400

    RELATORIO CONCLUÍDO

--- AppSysoHelp/Controllers/RelatoriosController.cs
+++ AppSysoHelp/Controllers/RelatoriosController.cs
@@ -16,24 +16,37 @@ namespace AppSysoHelp.Controllers
         {
             ViewBag.tipo = id;
             return View();
-        } 
+        }
 
         public IActionResult ChamadosPorPeriodo(string? tecnico, string? cliente, DateTime dataInicio, DateTime dataFinal)
         {
-            var chamadosFiltrados = _context.Chamados.Include(a => a.FkSituacaoChamado).Include(a => a.FkCliente).AsQueryable();
+            
 
-            chamadosFiltrados = chamadosFiltrados.Where(a => a.DataCriacao >= dataInicio && a.DataFechamento <= dataFinal);
-
-            if (!string.IsNullOrEmpty(tecnico))
-                chamadosFiltrados = chamadosFiltrados.Where(a => a.FkTecnico.PkId == Convert.ToInt64(tecnico));
+            var chamadosFiltrados = _context.Atendimentos.Include(a => a.FkTecnico)
+                                                         .Include(c => c.FkChamado).ThenInclude(c => c.FkSituacaoChamado)
+                                                         .Include(a => a.FkChamado).ThenInclude(c => c.FkCliente)
+                                                         .Where(a => a.DataFechamento >= dataInicio.Date && a.DataFechamento <= dataFinal.Date)
+                                                         .AsEnumerable();
+            var agrupamento = new List<Atendimentos>();
+            foreach (var item in chamadosFiltrados.GroupBy(a=> a.FkChamadoId))
+            {
+                var aten = item.OrderByDescending(a => a.AtendimentoId).FirstOrDefault();
+                agrupamento.Add(aten!);
+            }
 
             if (!string.IsNullOrEmpty(cliente))
-                chamadosFiltrados = chamadosFiltrados.Where(a => a.FkCliente.ClienteId == Convert.ToInt64(cliente));
+                agrupamento = agrupamento.Where(a => a.FkChamado.FkCliente.ClienteId == Convert.ToInt64(cliente)).ToList();
 
-            var resultados = chamadosFiltrados.ToList();
-            ViewBag.DataInicio = dataInicio.ToShortDateString(); ViewBag.DataFinal = dataFinal.ToShortDateString();
-            ViewBag.TotalChamados = resultados.Count();
-            return View(resultados);
+
+            if (!string.IsNullOrEmpty(tecnico))
+                agrupamento = agrupamento.Where(a => a.FkTecnicoId == Convert.ToInt64(tecnico)).ToList();
+
+
+            ViewBag.DataInicio = dataInicio.ToShortDateString();
+            ViewBag.DataFinal = dataFinal.ToShortDateString();
+            ViewBag.TotalChamados = agrupamento.Count();
+
+            return View(agrupamento);
         }
     }
 }
